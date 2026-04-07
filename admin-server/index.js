4bf766e529bfc2b0adda3a8cb430c53ff51a793e
Karthikeyasharma979 <karthikeyasharma979@gmail.com>
2026-04-07 06:57:59 +0530

    Added health check endpoint for easier server monitoring

--- admin-server/index.js
+++ admin-server/index.js
@@ -15,6 +15,7 @@ app.use(cors({
 app.use(express.json());
 
 // Routes
+app.get('/', (req, res) => res.json({ status: 'Server is running', service: 'AstroAdmin API' }));
 app.use('/api/admin', adminRoutes);
 
 // Detailed Request Logging Middleware (Development)
